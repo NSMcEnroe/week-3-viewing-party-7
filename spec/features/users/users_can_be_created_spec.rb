a86843462ebb3cadc99b2a0677ebb066c2d5bb13
Nicholas McEnroe <nsmcenro@gmail.com>
2023-10-23 19:33:02 -0600

    Feat: Add US 2

--- spec/features/users/users_can_be_created_spec.rb
+++ spec/features/users/users_can_be_created_spec.rb
@@ -19,4 +19,45 @@ RSpec.describe "User Registration form" do
 
     expect(current_path).to eq(user_path(user))
   end
+
+  it "rejects a new user if the user leaves any field blank" do
+    visit "/register"
+
+    name = ""
+    email = ""
+    password = ""
+
+    fill_in :user_name, with: name
+    fill_in :user_email, with: email
+    fill_in :user_password, with: password
+    fill_in :user_password_confirmation, with: password
+
+    click_on "Create New User"
+
+    user = User.last
+
+    expect(page).to have_content("Password can't be blank")
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Email can't be blank")
+  end
+
+  it "rejects a new user if the passwords don't match" do
+    visit "/register"
+
+    name = "Chuck"
+    email = "chuck@ymail.com"
+    password = "hey"
+    wrong_password = "you"
+
+    fill_in :user_name, with: name
+    fill_in :user_email, with: email
+    fill_in :user_password, with: password
+    fill_in :user_password_confirmation, with: wrong_password
+
+    click_on "Create New User"
+
+    user = User.last
+
+    expect(page).to have_content("Password confirmation doesn't match")
+  end
 end
