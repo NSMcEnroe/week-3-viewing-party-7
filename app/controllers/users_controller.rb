4b4b4fed5137a2ee27fb589186d0353817627290
Nicholas McEnroe <nsmcenro@gmail.com>
2023-10-24 12:11:16 -0600

    Feat: Add US 1 and most of US 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,12 @@ class UsersController <ApplicationController
   end 
 
   def create 
-    user = User.create(user_params)
-    if user.save
-      redirect_to user_path(user)
+    new_user = User.create(user_params)
+    if new_user.save
+      session[:user_id] = new_user.id
+      redirect_to user_path(new_user)
     else  
-      flash[:error] = user.errors.full_messages.to_sentence
+      flash[:error] = new_user.errors.full_messages.to_sentence
       redirect_to register_path
     end 
   end 
@@ -23,6 +24,7 @@ class UsersController <ApplicationController
   def login
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect_to user_path(user)
     else 
       flash[:error] =  "Your credentials are bad.  Try again."
